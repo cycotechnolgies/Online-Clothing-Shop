8b73f706d9c5df1775794bc2f94c2c188824e46d
nidula6 <nidulajayawardana@gmail.com>
2025-08-08 02:39:44 +0530

    add Footer and Header features

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 import './App.css'
-import Footer from './components/Footer.jsx'
+import HomeView from './pages/views/HomeView'
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <Footer />
+      <HomeView />
     </>
   )
 }
--- frontend/src/pages/views/HomeView.jsx
+++ frontend/src/pages/views/HomeView.jsx
@@ -1,7 +1,12 @@
+import Footer from "../../components/Footer.jsx"
+import Header from "../../components/Header.jsx"
+
+
 const HomeView = () => {
   return (
     <div className="bg-gray-50 min-h-screen">
-      
+      <Header />
+      <Footer />
     </div>
   )
 }
