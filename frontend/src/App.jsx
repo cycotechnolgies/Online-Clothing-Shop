0e3c108200d5b9020463b1f9e4e19a29af0f7c89
MadushanLSKK <Kmadushan396@gmail.com>
2025-08-07 20:27:03 +0530

    create basic login page with sample image

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import Login from './pages/views/login'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1 className='text-3xl font-bold underline text-center mt-10'>
-        Online-Clothing-Shop
-      </h1>
-      <button
-        onClick={() => setCount(count + 1)}
-        className='px-6 py-2 bg-blue-600 text-white font-semibold rounded-lg hover:bg-blue-700 transition duration-300'
-      >click me</button>
+      <Login/>
     </>
   )
 }
