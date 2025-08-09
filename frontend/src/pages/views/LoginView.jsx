dae1e367d649562c0b6c47776a576958b6402904
Shammi Nethupul <132045629+cycotechnolgies@users.noreply.github.com>
2025-08-09 20:53:41 +0530

    3 user authentication interface login page (#22)
    * create basic login page with sample image
    
    * Revert "create basic login page with sample image"
    
    This reverts commit 0e3c108200d5b9020463b1f9e4e19a29af0f7c89.
    
    reverting accidently mearged PR
    
    * Revert " Revert "create basic login page with sample image""
    
    This reverts commit a8fb98c3875e9b59ab7e7cba0be3e813f9c696f0.
    
    * Make Basic Routes for pages
    
    changed app.js, main.jsx and rename login to LoginView. added routes for navigation through pages
    
    * Login Screen UI updated (#17)
    
    change the login interface and use className attribute instead of class attribute
    
    Co-authored-by: Shammi Nethupul <132045629+cycotechnolgies@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: MadushanLSKK <Kmadushan396@gmail.com>
    Co-authored-by: Kavindu Madushan <158357717+MadushanLSKK@users.noreply.github.com>

--- frontend/src/pages/views/LoginView.jsx
+++ frontend/src/pages/views/LoginView.jsx
@@ -1,51 +1,61 @@
 import React from 'react'
 import image from '../../assets/image.png'
+import cart from '../../assets/cart.svg'
+import google from '../../assets/google.svg'
+import facebook from '../../assets/facebook.svg'
 
 const LoginView = () => {
   return (
-    <div class="lg:flex lg:items-center lg:justify-content-between lg:mx-auto lg:w-250 lg:gap-15 bg-gray-50 lg:mt-8">
-     <img 
-  src={image} 
-  alt="Your Company" 
-  className="hidden md:block mx-auto h-100 w-100 lg:flex" 
-/>
-
-      <div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8 ">
-        <div class="sm:mx-auto sm:w-full sm:max-w-sm">
-   
-        <h2 class="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900">Hello There!</h2>
-        <p class="mt-4 text-center text-xl/9 font-semiboldbold tracking-tight">welcome😀 you have been missed.please enter your data to log in</p>
+    <div className="lg:flex lg:items-center lg:justify-content-between lg:mx-auto lg:w-250  bg-gray-100 lg:mt-8 rounded-4xl ">
+      <div className='lg:ml-20 bg-linear-to-t from-cyan-500 to-blue-500 rounded-4xl hidden  mx-auto h-100 w-100 lg:flex '>
+        <img src={image} alt="Your Company" className=" " />
       </div>
+      
+      <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8 lg:w-150 ">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+          <div className='flex items-center justify-center gap-x-2'>
+            <img src={cart} alt="Your Company" className=" h-10 w-auto bg-blue-600 rounded-full p-2 " />
+            <p className='font-bold'>DN spurt</p>
+          </div>
+          <h2 className="mt-4 text-center text-3xl/9 font-bold tracking-tight text-gray-900">Wellcome back</h2>
+          <p className="mt-1 text-center text-sm font-semiboldbold tracking-tight text-gray-400">Please login to your account</p>
+       </div>
 
-      <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+       <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+
         <form action="#" method="POST" class="space-y-6">
          <div>
-          <label for="email" class="block text-sm/6 font-medium text-gray-900">Email address</label>
-          <div class="mt-2">
-            <input id="email" type="email" name="email" required autocomplete="email" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
+          <label for="email" className="block text-sm/6 font-medium text-gray-900">Email address</label>
+          <div className="mt-2">
+            <input id="email" type="email" name="email" required autocomplete="email" className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
           </div>
+         </div>
+
+         <div>
+          <div className="flex items-center justify-between">
+           <label for="password" className="block text-sm/6 font-medium text-gray-900">Password</label>
+           <div className="text-sm">
+              <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">Forgot password?</a>
+           </div>
+         </div>
+        <div className="mt-2">
+          <input id="password" type="password" name="password" required autocomplete="current-password" className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
+        </div>
       </div>
 
       <div>
-        <div class="flex items-center justify-between">
-          <label for="password" class="block text-sm/6 font-medium text-gray-900">Password</label>
-          <div class="text-sm">
-            <a href="#" class="font-semibold text-indigo-600 hover:text-indigo-500">Forgot password?</a>
-          </div>
-        </div>
-        <div class="mt-2">
-          <input id="password" type="password" name="password" required autocomplete="current-password" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
-        </div>
-      </div>
-
-      <div>
-        <button type="submit" class="flex w-full justify-center rounded-md bg-green-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-green-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-600">Sign in</button>
+        <button type="submit" className="flex w-full justify-center rounded-md bg-black px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs">Sign in</button>
       </div>
     </form>
+    <p class="mt-6 text-center text-sm/6 text-gray-400 flex items-center" ><hr className="w-20 border-t-2 border-gray-300 mx-auto" />Or Login With<hr className="w-20 border-t-2 border-gray-300 mx-auto" /></p>
+    <div className="mt-6 flex items-center justify-center gap-x-6 lg:justify-center ">
+         <button type="submit" className="flex w-full justify-center rounded-md bg-white px-1 py-1.5 text-sm/6 font-semibold text-black  shadow-xs focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-100 items-center justify-content-center gap-x-2"> <img src={google} className='h-4 w-auto'/> Google</button>
+          <button type="submit" className="flex w-full justify-center rounded-md bg-white px-1 py-1.5 text-sm/6 font-semibold text-black  shadow-xs focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-100 items-center justify-content-center gap-x-2"><img src={facebook} className='h-4 w-auto'/>Facebook</button>
+        </div>
 
-    <p class="mt-10 text-center text-sm/6 text-gray-500">
+    <p class="mt-10 text-center text-sm/6 text-gray-400">
       Don't have an account?
-      <a href="#" class="font-semibold text-indigo-600 hover:text-indigo-500"> Sign Up</a>
+      <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500"> Sign Up</a>
     </p>
   </div>
 </div>
