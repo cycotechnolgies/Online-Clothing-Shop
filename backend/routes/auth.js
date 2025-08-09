79bdca37aaded6bfe3116e806a1d09fabcfffff4
MiniduOshan <126508461+MiniduOshan@users.noreply.github.com>
2025-08-09 22:39:22 +0530

    Auth done and checked API

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,9 +1,20 @@
 const express = require("express");
-const { login, logoutUser } = require("../controller/authController");
 const router = express.Router();
+const requireAuth = require('../middleware/authMiddleware');
+const allowRoles = require('../middleware/roleMiddleware');
+const { register, login, logoutUser } = require('../controller/authController');
 
 // POST route for login
+router.post("/register", register);
 router.post("/login", login);
 router.post("/logout", logoutUser);
+
+
+// Example protected route (admin only)
+router.get('/admin-only', requireAuth, allowRoles('admin'), (req, res) => {
+  res.json({ message: 'Hello Admin!' });
+});
+
+router.post('/logout', requireAuth, logoutUser);
 
 module.exports = router;
