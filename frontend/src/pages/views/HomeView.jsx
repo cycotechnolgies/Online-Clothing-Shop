b0fc52eee8cd3452ce26f574d5bab0575fc37158
kavinda2024 <kavindanelum.kill@gmail.com>
2025-08-09 14:00:16 +0530

    created initial Featured products Section

--- frontend/src/pages/views/HomeView.jsx
+++ frontend/src/pages/views/HomeView.jsx
@@ -1,12 +1,14 @@
 import Footer from "../../components/Footer.jsx"
 import Header from "../../components/Header.jsx"
+import FeaturedProducts from "../../components/FeaturedProducts"
 
 
 const HomeView = () => {
   return (
     <div className="bg-gray-50 min-h-screen">
       <Header />
-      <Footer />
+      <FeaturedProducts/>
+       <Footer />
     </div>
   )
 }
